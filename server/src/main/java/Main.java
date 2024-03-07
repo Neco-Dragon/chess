d6aea056bcb232718ec52b0f62d46bf9ba46303e
rowan-williams <rowanwilliams263@gmail.com>
2024-03-07 08:04:35 -0700

    Project 4: Threw Data Access Exception across Main, Server, and PersistenceTests

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,9 @@
+import Exceptions.DataAccessException;
 import chess.*;
 import server.Server;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DataAccessException {
         Server myServer = new Server();
         myServer.run(8080);
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
--- server/src/test/java/passoffTests/serverTests/StandardAPITests.java
+++ server/src/test/java/passoffTests/serverTests/StandardAPITests.java
@@ -1,5 +1,6 @@
 package passoffTests.serverTests;
 
+import Exceptions.DataAccessException;
 import chess.ChessGame;
 import org.junit.jupiter.api.*;
 import passoffTests.obfuscatedTestClasses.TestServerFacade;
@@ -38,7 +39,7 @@ public class StandardAPITests {
 
 
     @BeforeAll
-    public static void init() {
+    public static void init() throws DataAccessException {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
