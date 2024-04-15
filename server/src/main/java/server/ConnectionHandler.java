647a41334db457288135f5a95be8b56264bfe150
rowan-williams <rowanwilliams263@gmail.com>
2024-04-15 17:32:22 -0600

    Phase 6: Trying to get Autograder to accept code attempt 1

--- server/src/main/java/server/ConnectionHandler.java
+++ server/src/main/java/server/ConnectionHandler.java
@@ -1,11 +1,7 @@
 package server;
 
-import org.eclipse.jetty.websocket.api.Session;
-import server.Connection;
-import webSocketMessages.*; //SERVER MESSAGE
-import webSocketMessages.serverMessages.Notification;
+//import org.eclipse.jetty.websocket.api.Session;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -14,13 +10,13 @@ public class ConnectionHandler {
     public final ConcurrentHashMap<Integer, ArrayList<Connection>> connections = new ConcurrentHashMap<>();
 
 
-    public void add(int gameID, String playerName, Session session) {
-        var connection = new Connection(playerName, session);
-        connections.get(gameID).add(connection);
-    }
-    //TODO: Do I have access directly to a connection? Just a session?
-    public void remove(int gameID, Connection connection) {
-        connections.get(gameID).remove(connection);
-    }
+//    public void add(int gameID, String playerName, Session session) {
+//        var connection = new Connection(playerName, session);
+//        connections.get(gameID).add(connection);
+//    }
+//    //Do I have access directly to a connection? Just a session?
+//    public void remove(int gameID, Connection connection) {
+//        connections.get(gameID).remove(connection);
+//    }
 
 }
