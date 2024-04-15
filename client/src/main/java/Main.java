f773d4180824f78e194575177c361e99a4b069af
rowan-williams <rowanwilliams263@gmail.com>
2024-04-15 12:12:53 -0600

    Phase 6: Trying to fix maven issues w/ TA

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,10 +1,8 @@
 import Repl.Repl;
-import server.Server;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("♕ 240 Chess Client: ");
-        Server server = new Server(); //TODO: Where do I need to stick my server?
         Repl repl = new Repl("http://localhost:8080"); //TODO: is this right?
         repl.run();
     }
