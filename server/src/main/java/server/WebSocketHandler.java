4bdd5edec41b21c7bbba5c02f63cde0f465773aa
rowan-williams <rowanwilliams263@gmail.com>
2024-04-15 14:19:06 -0600

    Phase 6: Constructor made for the WebSocketHandler class

--- server/src/main/java/server/WebSocketHandler.java
+++ server/src/main/java/server/WebSocketHandler.java
@@ -1,13 +1,33 @@
 package server;
 
+import Exceptions.*;
 import com.google.gson.Gson;
+import dataAccess.*;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import webSocketMessages.userCommands.UserGameCommand;
 
+import java.lang.reflect.GenericArrayType;
+
 @WebSocket
 public class WebSocketHandler {
+
+    GameDAO gameDAO;
+    UserDAO userDAO;
+    AuthDAO authDAO;
+    //TODO: Also keep a sessions object. Connection Handler?
+
+    public WebSocketHandler() {
+        try {
+            gameDAO = new MySQLGameDAO();
+            userDAO = new MySQLUserDAO();
+            authDAO = new MySQLAuthDAO();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
     @OnWebSocketMessage
     public void onMessage(Session session, String message) throws Exception {
         UserGameCommand userGameCommand = new Gson().fromJson(message, UserGameCommand.class);
@@ -21,6 +41,7 @@ public class WebSocketHandler {
         }
 
     }
+    //SERVICES WILL CALL THE DAOs
 
     private void joinPlayerService(Session session, String message) {
     }
