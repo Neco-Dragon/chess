d788c4d9e5d076c324469c940b5b8c3f6bf77a94
rowan-williams <rowanwilliams263@gmail.com>
2024-04-15 12:57:15 -0600

    Phase 6: Skeleton made for the WebSocketHandler class

--- server/src/main/java/server/WebSocketHandler.java
+++ server/src/main/java/server/WebSocketHandler.java
@@ -1,19 +1,37 @@
 package server;
 
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import webSocketMessages.userCommands.UserGameCommand;
 
 @WebSocket
 public class WebSocketHandler {
     @OnWebSocketMessage
     public void onMessage(Session session, String message) throws Exception {
-        //Serialized JSON, one of 5 UserGameCommands
-        //Case:
-        // Gson to Json
-        // Forward it to one of the 5 userGame Command Service Classes
-        System.out.printf("Received: %s", message);
-        session.getRemote().sendString("WebSocket response: " + message);
+        UserGameCommand userGameCommand = new Gson().fromJson(message, UserGameCommand.class);
+
+        switch (userGameCommand.getCommandType()){
+            case JOIN_PLAYER -> this.joinPlayerService(session, message);
+            case JOIN_OBSERVER -> this.joinObserverService(session, message);
+            case MAKE_MOVE -> this.makeMoveService(session, message);
+            case RESIGN -> this.resignService(session, message);
+            case LEAVE -> this.leaveService(session, message);
+        }
+
     }
+
+    private void joinPlayerService(Session session, String message) {
+    }
+    private void joinObserverService(Session session, String message) {
+    }
+    private void makeMoveService(Session session, String message) {
+    }
+    private void resignService(Session session, String message) {
+    }
+    private void leaveService(Session session, String message) {
+    }
+
 
 }
